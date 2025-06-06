28736c70cb617c6a395f4e0108b3c7194896cd03
aspogorelova <anyapogorelova17@gmail.com>
2025-06-06 10:36:14 +0300

    correct eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
+  { ignores: ['node_modules', 'dist', 'webpack.config.js'] },
   stylistic.configs.recommended,
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: { ...globals.browser, ...globals.node, ...globals.jest } } },
 ])
